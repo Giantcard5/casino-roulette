da036620a5d0ecf25d0e1b8c6cadd5e2b3e21d31
giantcard5 <giantcard5.dev@outlook.com>
2024-08-29 22:51:13 -0300

    Create Repository

---
+++ src/core/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+
+    flex-direction: column;
+
+    width: 100vw;
+    height: 100vh;
+
+    background-color: #EFEFEF;
+`;
